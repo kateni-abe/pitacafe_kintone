3571ff20b54b91ab7f46fa44a29fb32fcb25b35b
kateni_abe <131324400+kateni-abe@users.noreply.github.com>
2025-07-22 22:49:05 +0900

    Update communication_log.js

--- js/communication-log/communication_log.js
+++ js/communication-log/communication_log.js
@@ -15,12 +15,14 @@
       height: '35px',
       width: 'auto',
       minWidth: '100px',
-      borderRadius: '10px 10px 0px 0px',
+      borderRadius: '4px 4px 4px 4px',
       backgroundColor: 'white',
       border: '1px solid #CCCCCC',
-      fontSize: '13px',
+      fontSize: '12px',
       paddingLeft: '10px',
       paddingRight: '10px',
+      marginLeft: '1px',
+      marginRight: '1px',
       cursor: 'pointer',
       flexShrink: 0
     });
